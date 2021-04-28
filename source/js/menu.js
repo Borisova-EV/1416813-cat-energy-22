7d80a1b17259fecb6bca8b44e9f50e689953d06b
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-04-28 15:52:47 +0300

    Добавляет изменение атрибута aria-label при клике на кнопку мобильного меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -6,5 +6,10 @@ navMenu.classList.remove('main-navigation--nojs');
 buttonMenu.addEventListener('click', function () {
   navMenu.classList.toggle('main-navigation--closed');
   navMenu.classList.toggle('main-navigation--opened');
+  if (navMenu.classList.contains('main-navigation--opened')) {
+    buttonMenu.setAttribute('aria-label', 'Закрыть меню');
+  } else {
+    buttonMenu.setAttribute('aria-label', 'Открыть меню');
+  }
   }
 );
