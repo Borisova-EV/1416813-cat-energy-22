c254c78d14ad4be1bf1327f04f636fbd3e23b62b
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-05-01 17:52:55 +0300

    Вносит изменения по ПП для планшетной и десктопной версии главной страницы

--- source/js/slider.js
+++ source/js/slider.js
@@ -3,25 +3,68 @@ let sliderButtonAfter = document.querySelector('.slider__button--after');
 let sliderRange = document.querySelector('.slider__range');
 let sliderContentBefore = document.querySelector('.slider__img--before');
 let sliderContentAfter = document.querySelector('.slider__img--after');
+let sliderContentMedium = document.querySelector('.slider__img--container');
+let smallDevice = window.matchMedia('(max-width: 767px)');
 
-sliderButtonAfter.addEventListener('click', function() {
-  sliderContentBefore.classList.remove('slider__img--display');
-  sliderContentAfter.classList.add('slider__img--display');
-  sliderRange.value = 2;
-});
 
-sliderButtonBefore.addEventListener('click', function() {
-  sliderContentAfter.classList.remove('slider__img--display');
-  sliderContentBefore.classList.add('slider__img--display');
-  sliderRange.value = 1;
-});
 
-sliderRange.addEventListener('change', function() {
-  if (sliderRange.value == 2) {
-    sliderContentBefore.classList.remove('slider__img--display');
-    sliderContentAfter.classList.add('slider__img--display');
-  } else {
-    sliderContentAfter.classList.remove('slider__img--display');
-    sliderContentBefore.classList.add('slider__img--display');
-  }
-})
+  smallDevice.addListener(handleDeviceChange);
+  function handleDeviceChange(e) {
+    if (e.matches) {
+      sliderButtonAfter.addEventListener('click', function() {
+        sliderContentBefore.classList.remove('slider__img--display');
+        sliderContentAfter.classList.add('slider__img--display');
+        sliderRange.value = 2;
+      });
+
+      sliderButtonBefore.addEventListener('click', function() {
+        sliderContentAfter.classList.remove('slider__img--display');
+        sliderContentBefore.classList.add('slider__img--display');
+        sliderRange.value = 1;
+      });
+
+      sliderRange.addEventListener('change', function() {
+        if (sliderRange.value == 2) {
+          sliderContentBefore.classList.remove('slider__img--display');
+          sliderContentAfter.classList.add('slider__img--display');
+        } else {
+          sliderContentAfter.classList.remove('slider__img--display');
+          sliderContentBefore.classList.add('slider__img--display');
+          }
+        });
+    } else {
+      sliderRange.setAttribute('max','3');
+      sliderContentBefore.classList.remove('slider__img--display');
+      sliderContentMedium.classList.add('slider__img--display');
+
+      sliderButtonAfter.addEventListener('click', function() {
+        sliderContentBefore.classList.remove('slider__img--display');
+        sliderContentMedium.classList.remove('slider__img--display');
+        sliderContentAfter.classList.add('slider__img--display');
+        sliderRange.value = 3;
+      });
+
+      sliderButtonBefore.addEventListener('click', function() {
+        sliderContentAfter.classList.remove('slider__img--display');
+        sliderContentMedium.classList.remove('slider__img--display');
+        sliderContentBefore.classList.add('slider__img--display');
+        sliderRange.value = 1;
+      });
+
+      sliderRange.addEventListener('change', function() {
+        if (sliderRange.value == 3) {
+          sliderContentBefore.classList.remove('slider__img--display');
+          sliderContentMedium.classList.remove('slider__img--display');
+          sliderContentAfter.classList.add('slider__img--display');
+        } else if (sliderRange.value == 2) {
+          sliderContentAfter.classList.remove('slider__img--display');
+          sliderContentBefore.classList.remove('slider__img--display');
+          sliderContentMedium.classList.add('slider__img--display');
+        } else {
+          sliderContentMedium.classList.remove('slider__img--display');
+          sliderContentAfter.classList.remove('slider__img--display');
+          sliderContentBefore.classList.add('slider__img--display');
+        }
+      });
+      }
+    }
