ccbbb18dedb9eb94c838c5970e37e098f2c6406b
Borisova-EV <72605569+Borisova-EV@users.noreply.github.com>
2021-05-11 06:09:15 +0300

    Декоративные элементы главной страницы (#11)
    * Добавляет фиксированные размеры контентных изображений для моб версии главной страницы
    
    * Добавляет стили и скрипт для мобильного меню на главной странице
    
    * Добавляет изменение атрибута aria-label при клике на кнопку мобильного меню
    
    * Правки по пп мобильной версии главной страницы
    
    * Добавляет управление слайдом
    
    * Вносит изменения по ПП для планшетной и десктопной версии главной страницы
    
    * Правки в блоке пример
    
    * Изменения для FF
    
    * Исправляет отступы для блока с адресом
    
    * Правки после ревью кода
    
    * Добавлят паддинги для ползунка
    
    * Делает исправления в js файлах после ревью
    
    * Изменяет js slider
    
    * Небольшие правки в slider.js

---
+++ source/js/menu.js
@@ -0,0 +1,14 @@
+let buttonMenu = document.querySelector('.main-navigation__button');
+let navMenu = document.querySelector('.main-navigation');
+
+navMenu.classList.remove('main-navigation--nojs');
+
+buttonMenu.addEventListener('click', function () {
+  navMenu.classList.toggle('main-navigation--closed');
+  navMenu.classList.toggle('main-navigation--opened');
+  if (navMenu.classList.contains('main-navigation--opened')) {
+    buttonMenu.setAttribute('aria-label', 'Закрыть меню');
+  } else {
+    buttonMenu.setAttribute('aria-label', 'Открыть меню');
+  }
+});
---
+++ source/js/slider.js
@@ -0,0 +1,81 @@
+let sliderButtonBefore = document.querySelector('.slider__button--before');
+let sliderButtonAfter = document.querySelector('.slider__button--after');
+let sliderRange = document.querySelector('.slider__range');
+let sliderContentBefore = document.querySelector('.slider__img--before');
+let sliderContentAfter = document.querySelector('.slider__img--after');
+let sliderContentMedium = document.querySelector('.slider__img--medium');
+let smallDevice = window.matchMedia('(max-width: 767px)');
+
+function sliderControl(smallDevice) {
+  if (smallDevice.matches) {
+    sliderRange.setAttribute('max', '2');
+    sliderContentMedium.classList.remove('slider__img--display');
+    sliderContentAfter.classList.remove('slider__img--display');
+    sliderRange.value = 1;
+    sliderContentBefore.classList.add('slider__img--display');
+
+    sliderButtonAfter.addEventListener('click', function () {
+      sliderContentBefore.classList.remove('slider__img--display');
+      sliderContentAfter.classList.add('slider__img--display');
+      sliderContentMedium.classList.remove('slider__img--display');
+      sliderRange.value = 2;
+    });
+
+    sliderButtonBefore.addEventListener('click', function () {
+      sliderContentAfter.classList.remove('slider__img--display');
+      sliderContentBefore.classList.add('slider__img--display');
+      sliderContentMedium.classList.remove('slider__img--display');
+      sliderRange.value = 1;
+    });
+
+    sliderRange.addEventListener('change', function () {
+      if (sliderRange.value == 2) {
+        sliderContentBefore.classList.remove('slider__img--display');
+        sliderContentMedium.classList.remove('slider__img--display');
+        sliderContentAfter.classList.add('slider__img--display');
+      } else {
+        sliderContentAfter.classList.remove('slider__img--display');
+        sliderContentBefore.classList.add('slider__img--display');
+        sliderContentMedium.classList.remove('slider__img--display');
+      }
+    });
+  } else {
+    sliderRange.setAttribute('max', '3');
+    sliderRange.value = 2;
+    sliderContentMedium.classList.add('slider__img--display');
+    sliderContentAfter.classList.remove('slider__img--display');
+    sliderContentBefore.classList.remove('slider__img--display');
+
+    sliderButtonAfter.addEventListener('click', function () {
+      sliderContentBefore.classList.remove('slider__img--display');
+      sliderContentMedium.classList.remove('slider__img--display');
+      sliderContentAfter.classList.add('slider__img--display');
+      sliderRange.value = 3;
+    });
+
+    sliderButtonBefore.addEventListener('click', function () {
+      sliderContentAfter.classList.remove('slider__img--display');
+      sliderContentMedium.classList.remove('slider__img--display');
+      sliderContentBefore.classList.add('slider__img--display');
+      sliderRange.value = 1;
+    });
+
+    sliderRange.addEventListener('change', function () {
+      if (sliderRange.value == 3) {
+        sliderContentBefore.classList.remove('slider__img--display');
+        sliderContentMedium.classList.remove('slider__img--display');
+        sliderContentAfter.classList.add('slider__img--display');
+      } else if (sliderRange.value == 2) {
+        sliderContentAfter.classList.remove('slider__img--display');
+        sliderContentBefore.classList.remove('slider__img--display');
+        sliderContentMedium.classList.add('slider__img--display');
+      } else {
+        sliderContentMedium.classList.remove('slider__img--display');
+        sliderContentAfter.classList.remove('slider__img--display');
+        sliderContentBefore.classList.add('slider__img--display');
+      }
+    });
+  }
+};
+sliderControl(smallDevice);
+smallDevice.addListener(sliderControl);
