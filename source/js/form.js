d75ab6d5d2768ff78f883d195870d58d8f07bc54
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-05-13 10:36:37 +0300

    Правки после ревью в form.js

--- source/js/form.js
+++ source/js/form.js
@@ -1,12 +1,12 @@
 let formSubmit = document.querySelector(".form__button");
-let formInputs = document.querySelectorAll("input[required");
+let formInputs = document.querySelectorAll("input[required]");
 
 formInputs.forEach(function (formInput) {
   formSubmit.addEventListener('click', function () {
     if (!formInput.value) {
-    formInput.classList.add('form__input--error');
-  } else {
-    formInput.classList.remove('form__input--error');
-  }
-})
+      formInput.classList.add('form__input--error');
+    } else {
+      formInput.classList.remove('form__input--error');
+    }
+  })
 })
